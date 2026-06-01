9af398f38b761c2129b7b55c4752a4ef3f037504
FSI001 <112025335+FSI001@users.noreply.github.com>
2026-06-01 23:21:59 +0600

    Add files via upload

--- Farshid Saleh Ivo - portfolio v1/netlify/functions/subscribers.js
+++ Farshid Saleh Ivo - portfolio v1/netlify/functions/subscribers.js
@@ -1,33 +1,28 @@
 exports.handler = async () => {
     const APIkey = process.env.YOUTUBE_API_KEY;
-    const youtubeID = "UCwjhIjcKGbFv_1wh8PyUExQ";
+    const channel1 = "UCwjhIjcKGbFv_1wh8PyUExQ";
+    const channel2 = "UCsgjaqe0hiWxc_VsA4uqF2Q";
 
     try {
         const response = await fetch(
-            `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeID}&key=${APIkey}`
+            `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channel1},${channel2}&key=${APIkey}`
         );
 
         const data = await response.json();
 
         return {
             statusCode: 200,
-            headers: {
-                "Content-Type": "application/json"
-            },
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                subscriberCount:
-                    data.items[0].statistics.subscriberCount
+                subscriberCount: data.items[0].statistics.subscriberCount,
+                subscriberCount2: data.items[1].statistics.subscriberCount
             })
         };
     } catch (error) {
         return {
             statusCode: 500,
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                error: "Failed to fetch subscriber count"
-            })
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ error: "Failed to fetch subscriber count" })
         };
     }
 };
