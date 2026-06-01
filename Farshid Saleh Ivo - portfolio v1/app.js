77b78b3c715489e6ac838b0dbef4e68c7385028f
FSI001 <112025335+FSI001@users.noreply.github.com>
2026-06-01 23:21:43 +0600

    Add files via upload

--- Farshid Saleh Ivo - portfolio v1/app.js
+++ Farshid Saleh Ivo - portfolio v1/app.js
@@ -1,54 +1,16 @@
 const subCount = document.querySelector(".sub-count");
+const subCount2 = document.querySelector(".sub-count-2");
 
 const getYouTubeSubs = async () => {
     try {
-        const response = await fetch(
-            "/.netlify/functions/subscribers"
-        );
-
+        const response = await fetch("/.netlify/functions/subscribers");
         const data = await response.json();
 
-        subCount.innerHTML =
-            Number(data.subscriberCount).toLocaleString();
+        subCount.innerHTML = Number(data.subscriberCount).toLocaleString();
+        subCount2.innerHTML = Number(data.subscriberCount2).toLocaleString();
     } catch (error) {
         console.error(error);
-
-        subCount.innerHTML =
-            "Error loading subscriber count";
+        subCount.innerHTML = "Error loading subscriber count";
+        subCount2.innerHTML = "Error loading subscriber count";
     }
 };
-
-getYouTubeSubs();
-
-document
-    .getElementById("hamburgerMenu")
-    .addEventListener("click", function () {
-        const navbar =
-            document.getElementById("navbar");
-
-        navbar.classList.toggle("showNav");
-    });
-
-function toggleSkills() {
-    const skillsList =
-        document.getElementById("skillsList");
-
-    const toggleButton =
-        document.getElementById("toggleButton");
-
-    if (
-        skillsList.classList.contains("collapsed")
-    ) {
-        skillsList.classList.remove("collapsed");
-        skillsList.classList.add("expanded");
-
-        toggleButton.textContent =
-            "Hide Skills";
-    } else {
-        skillsList.classList.remove("expanded");
-        skillsList.classList.add("collapsed");
-
-        toggleButton.textContent =
-            "Show Skills";
-    }
-}
